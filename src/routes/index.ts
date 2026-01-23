6a51701105e90212ad1e92cebd6890bc38855750
BoneDeath <mhasannudin300@gmail.com>
2026-01-23 15:09:36 +0700

    add builder schema end point

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,28 +1,15 @@
 import { Router } from 'express'
 
-// ===== AUTH ROUTES =====
-import authRoutes from './auth.routes'
-
-// ===== MASTER DATA ROUTES =====
+import authRoutes from './auth/auth.routes'
 import productRoutes from './master/masters.product.routes'
 import userRoutes from './master/masters.user.routes'
+import builderRoutes from './builder/builder.routes'
 
 const router = Router()
 
-/**
- * =========================
- * AUTH
- * =========================
- */
 router.use('/auth', authRoutes)
-
-/**
- * =========================
- * MASTER DATA
- * =========================
- */
 router.use('/master/products', productRoutes)
 router.use('/master/users', userRoutes)
-
+router.use('/builder/schema', builderRoutes)
 
 export default router
