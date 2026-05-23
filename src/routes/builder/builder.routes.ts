8e5b0b1f3061ba2e670ba873456b4359da1f745a
BoneDeath <mhasannudin300@gmail.com>
2026-05-23 17:00:52 +0700

    update

--- src/routes/builder/builder.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import builderController from "../../controllers/builder.controller";
-import authMiddleware from "../../middleware/auth.middleware";
-
-const router = Router();
-
-router.get("/", authMiddleware, builderController.getSchema);
-export default router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,13 +3,11 @@ import { Router } from 'express'
 import authRoutes from './auth/auth.routes'
 import productRoutes from './master/masters.product.routes'
 import userRoutes from './master/masters.user.routes'
-import builderRoutes from './builder/builder.routes'
 
 const router = Router()
 
 router.use('/auth', authRoutes)
 router.use('/master/products', productRoutes)
 router.use('/master/users', userRoutes)
-router.use('/builder/schema', builderRoutes)
 
 export default router
